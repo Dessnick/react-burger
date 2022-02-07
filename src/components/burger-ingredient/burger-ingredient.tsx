37011d3bc07862eacbec8ac6025c75dec5ba2f7a
Denis Semes <dessnick@gmail.com>
2022-02-08 00:25:27 +0700

    fix: refactoring

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -11,31 +11,29 @@ const BurgerIngredient = ({ ingredientName, ingredientList, tabRef }) => {
     <div ref={tabRef}>
       <h2 className="text text_type_main-medium">{ingredientName}</h2>
       <ul className={`${styles.ingredients_list} + pt-6 pb-10 pl-4 pr-4`}>
-        {ingredientList.map((item) => {
-          return (
-            <li className={styles.item} key={item._id}>
-              <a href="#" className={`${styles.link} + mb-8`}>
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className={`${styles.image} + pl-4 pr-4`}
-                />
-                <div className={`${styles.price_container} + pt-1 pb-1`}>
-                  <p className="text text_type_digits-default mr-2">
-                    {item.price}
-                  </p>
-                  <CurrencyIcon type="primary" />
-                </div>
-                <p
-                  className={`${styles.caption} + "text text_type_main-default mb-6"`}
-                >
-                  {item.name}
+        {ingredientList.map((item) => (
+          <li className={styles.item} key={item._id}>
+            <a href="#" className={`${styles.link} + mb-8`}>
+              <img
+                src={item.image}
+                alt={item.name}
+                className={`${styles.image} + pl-4 pr-4`}
+              />
+              <div className={`${styles.price_container} + pt-1 pb-1`}>
+                <p className="text text_type_digits-default mr-2">
+                  {item.price}
                 </p>
-                <Counter count={0} size="default"></Counter>
-              </a>
-            </li>
-          );
-        })}
+                <CurrencyIcon type="primary" />
+              </div>
+              <p
+                className={`${styles.caption} + "text text_type_main-default mb-6"`}
+              >
+                {item.name}
+              </p>
+              <Counter count={1} size="default"></Counter>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
